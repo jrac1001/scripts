c479c064ccc4a13b8135b0e4b6178327ca350bd1
Claim created by JAMES BARRETT RACSTER on 12/12/2024 <29495698+jrac1001@users.noreply.github.com>
2025-07-19 21:00:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,30 +11,27 @@ sudo systemctl enable acpid
 sudo apt install -y alsa-utils volumeicon-alsa
 sudo apt install -y lightdm-gtk-greeter-settings
 sudo systemctl enable lightdm
-sudo apt install -y build-essential uv curl wget
-sudo apt install -y micro nala ffmpeg zoxide fzf bat ranger git ncdu vnstat btop mpv foliate
+sudo apt install -y pipx curl wget
+sudo apt install -y ffmpeg zoxide fzf bat ranger git ncdu vnstat btop mpv foliate
 curl -fsS https://dl.brave.com/install.sh | sh
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/v2.43.0/fastfetch-linux-amd64.deb
 sudo dpkg -i fastfetch-linux-amd64.deb
-curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
-echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt update
-sudo apt install -y eza
-sudo apt install gnome-software-plugin-flatpak
+sudo apt install -y eza flatpak 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-# flatpak install flathub org.wezfurlong.wezterm
 sudo apt install -y cups bluez blueman system-config-printer simple-scan
 sudo systemctl enable cups.service
 sudo systemctl enable bluetooth
 sudo systemctl enable cups
-sudo apt install -y hplip hplip-gui conky-all
-sudo apt install -y printer-driver-hpcups retext
-sudo apt install -y geany geany-plugins vlc yt-dlp ripgrep xpad timeshift
-sudo apt install -y xfce4-panel-profiles xfconf-query devilspie2
+sudo apt install -y hplip hplip-gui conky-all redshift xprop
+sudo apt install -y printer-driver-hpcups retext yad openvpn
+sudo apt install -y geany geany-plugins vlc yt-dlp ripgrep xpad 
+sudo apt install -y xfconfd devilspie2
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install --git https://gitlab.torproject.org/tpo/core/oniux --tag v0.4.0 oniux
+sudo apt install -y abiword gnumeric apt-listbugs
 
